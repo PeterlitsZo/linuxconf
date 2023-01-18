887fefe465074a76513bae280cee4b371bc75d8c
Peterlits Zo <peterlitszo@gmail.com>
2023-01-18 19:39:17 +0800

    Update document for neovim

--- install.sh
+++ install.sh
@@ -22,12 +22,17 @@ elif [[ "$1" == "install" && "$2" == "neovim" ]]; then
             echo
             echo "Follow this document:"
             echo
-            echo '  1. Use `curl -sL install-node.now.sh/lts | bash` to install node for coc.nvim'
-            echo '  2. Follow the document to install vim-plug: https://github.com/junegunn/vim-plug'
+            echo '  1. Install Node.js:'
+            echo
+            echo '    1.1. Check to make sure you have Node.js by `node -v`. If you have, skip to install Node.js.'
+            echo '    1.2. Use `curl -sL install-node.now.sh/lts | bash` to install Node.js for coc.nvim'
+            echo
+            echo '  2. Follow the document to install vim-plug: https://github.com/junegunn/vim-plug#installation'
             echo "  3. Run command in nvim:"
             echo
             echo '    3.1. `:PlugInstall`'
-            echo '    3.2. `:CocInstall coc-git coc-java`'
+            echo '    3.2. `:CocInstall <extensions>`. Get coc extensions here:'
+            echo '         https://github.com/neoclide/coc.nvim/wiki/Using-coc-extensions#implemented-coc-extensions'
             echo
         else
             echo "File ~/.config/nvim/init.vim exists. Skip."
